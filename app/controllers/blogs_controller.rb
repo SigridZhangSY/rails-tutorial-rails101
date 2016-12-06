ca3b200f21ff045627584a7d6637d54d835b4020
SigridZhangSY <sigridzhang0603@163.com>
2016-12-06 15:02:45 +0800

    create blog of a group

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,25 @@
 class BlogsController < ApplicationController
+  before_filter :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @blog = Blog.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @blog = Blog.new(blog_params)
+    @blog.group = @group
+    @blog.user = current_user
+    if @blog.save
+      redirect_to groups_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:content)
+  end
 end
