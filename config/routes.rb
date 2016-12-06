5b54dc267d2264ef206dc3221621274cc698efa1
SigridZhangSY <sigridzhang0603@163.com>
2016-12-06 14:23:24 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails101::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :groups
+  resources :groups do
+    resources :blogs
+  end
   # You can have the root of your site routed with "root"
   root 'groups#index'
 
